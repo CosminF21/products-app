bbfeb58f8d533159d99d6487a1a11e2ed2da7934
Cosmin <cosmincofan@yahoo.com>
2026-01-14 13:11:58 +0200

    fix unit test - use h2 db

--- src/test/java/com/example/products_app/product/unit/ProductControllerUnitTest.java
+++ src/test/java/com/example/products_app/product/unit/ProductControllerUnitTest.java
@@ -9,9 +9,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ProductControllerUnitTest {
 
     @Autowired
@@ -34,7 +36,7 @@ class ProductControllerUnitTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private ProductService productService;
 
     private List<Product> products;
