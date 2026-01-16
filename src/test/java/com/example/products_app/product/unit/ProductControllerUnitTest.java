74039ca934982ed185798c5e7d23f8f288281795
Cosmin <cosmincofan@yahoo.com>
2026-01-16 15:33:32 +0200

    fix unit test

--- src/test/java/com/example/products_app/product/unit/ProductControllerUnitTest.java
+++ src/test/java/com/example/products_app/product/unit/ProductControllerUnitTest.java
@@ -1,14 +1,16 @@
 package com.example.products_app.product.unit;
 
+import com.example.products_app.config.SecurityConfig;
 import com.example.products_app.product.Product;
+import com.example.products_app.product.ProductController;
 import com.example.products_app.product.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,8 +27,8 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ProductController.class)
+@Import(SecurityConfig.class)
 @ActiveProfiles("test")
 class ProductControllerUnitTest {
 
